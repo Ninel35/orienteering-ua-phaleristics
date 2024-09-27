e39afc1383e6959834b181edade63045e8439a27
Ninel35 <ninarasch3@gmail.com>
2024-09-27 20:42:10 +0200

    react-router setup, sidebar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Navigation from "../Navigation/Navigation"
+
+const Header = ()=> {
+    return (
+        <header>
+            {/* <h1>Orienteerng</h1> */}
+            <Navigation></Navigation>
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,36 @@
+import {  NavLink } from "react-router-dom"
+import css from "./Navigation.module.css"
+import { useState } from "react"
+
+
+ const Navigation = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  return (
+    <nav>
+     {isSidebarOpen && <ul className={css.sidebar}>
+      <li onClick={()=>{setIsSidebarOpen(false)}}className={css.closeBtn}><NavLink><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z"/></svg></NavLink></li>
+        <li>
+          <NavLink to="/">Home</NavLink></li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/news">News</NavLink>
+          </li>
+      </ul>}
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink></li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/news">News</NavLink>
+          </li>
+        <li onClick={()=>{setIsSidebarOpen(true)}}><NavLink><svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 -960 960 960" width="26px" fill="#5f6368"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></NavLink></li>
+      </ul>
+    </nav>
+  )
+}
+export default Navigation
