000d7a7ce3a6bdf1e8f4195e0bce9778e816f6f1
Ninel35 <ninarasch3@gmail.com>
2024-12-07 21:13:39 +0100

    updateCardItem

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -4,13 +4,13 @@ import css from "./CardItem.module.css";
 const CardItem = ({ result, location }) => {
   return (
     <li className={css.card}>
-      <p>{result.title}</p>
-      <img
-        className={css.picture}
-        src={result.photo[0]?.url}
-        alt={result.title}
-        // width="150"
-      />
+        <p className={css.titleComp}>{result.title}</p>
+        <img
+          className={css.picture}
+          src={result.photo[0]?.url}
+          alt={result.title}
+        />
+         <p className={css.titleMobile}>{result.title}</p>
       <Link to={result.id} className={css.readMoreBtn} state={{backLink:location}}>Read more</Link>
     </li>
   );
