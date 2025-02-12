1eb3ddfc7d9320a83ccd05da577fac95efc7261b
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2025-02-12 20:49:11 +0100

    header css update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
-import Navigation from "../Navigation/Navigation"
-import css from "./Header.module.css"
-const Header = ()=> {
-    return (
-        <header>
-            <h1>Phaleristics of Orienteering.UA</h1>
-            <Navigation></Navigation>
-        </header>
-    )
-}
-export default Header
+import Navigation from "../Navigation/Navigation";
+import css from "./Header.module.css";
+const Header = () => {
+  return (
+    <header className={css.header}>
+      <h1>Phaleristics of Orienteering.UA</h1>
+      <Navigation></Navigation>
+    </header>
+  );
+};
+export default Header;
