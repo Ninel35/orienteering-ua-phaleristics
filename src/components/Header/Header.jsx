c4ac1de17050d9a6d5e360c6b1e39ee2b5e1527c
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2025-02-14 21:42:18 +0100

    icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import css from "./Header.module.css";
 const Header = () => {
   return (
     <header className={css.header}>
-      <h1>Phaleristics of Orienteering.UA</h1>
+      <h1>Orienteering History of Ukraine</h1>
       <Navigation></Navigation>
     </header>
   );
