df53334ed1cb7e625836a93db3150e7dd69bf287
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2025-02-20 15:25:00 +0100

    fixing mistakes

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -1,6 +1,12 @@
+import { Outlet } from "react-router-dom";
 import NewsList from "../../components/NewsList/NewsList";
 
 const News = () => {
-  return <NewsList />;
+  return (
+    <>
+      <NewsList />
+      <Outlet />
+    </>
+  );
 };
 export default News;
