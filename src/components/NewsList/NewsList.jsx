40869276886fb4c6c0b64142a7eb317189d900a9
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2025-02-20 19:27:16 +0100

    fixing mistakes

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,16 +1,20 @@
 import css from "./NewsList.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import NewsItem from "../NewsItem/NewsItem";
+import { useEffect } from "react";
+import { fetchLatestNewses } from "../../redux/latestSlice";
 
 const NewsList = ({ location }) => {
   const news = useSelector((state) => state.latestNews);
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const scrollPosition = sessionStorage.getItem("scrollPosition");
-  //   if (scrollPosition) {
-  //     window.scrollTo(0, parseInt(scrollPosition, 10));
-  //   }
-  // }, []);
+  useEffect(() => {
+    dispatch(fetchLatestNewses());
+    // const scrollPosition = sessionStorage.getItem("scrollPosition");
+    // if (scrollPosition) {
+    //   window.scrollTo(0, parseInt(scrollPosition, 10));
+    // }
+  }, []);
 
   if (news.loading) return <div>Завантаження... </div>;
   if (news.error) return <div>Виникла помилка</div>;
